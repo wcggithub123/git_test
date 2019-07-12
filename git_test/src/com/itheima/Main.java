31d063a37d7ed894715e80649f7abd17a8223414
wang <846045111@qq.com>
2019-07-12 21:04:24 +0800

    把本地工程添加到git

--- git_test/src/com/itheima/Main.java
+++ git_test/src/com/itheima/Main.java
@@ -9,4 +9,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
+    public void test(){
+        int i = 10;
+        System.out.println(i);
+    }
 }
